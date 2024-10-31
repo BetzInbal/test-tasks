96ceca9f920db68fde0b76fe15c32136e6d02a51
betz <betzinbal@gmail.com>
2024-10-31 11:12:45 +0200

    base structure complited

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Add from './components/Add'
+import Grid from './components/Grid'
+import Task from './types/task'
 
 function App() {
 
+  const [tasks, setTasks] = useState<Task[]>()
+  
   return (
     <>
-
+    <Add/>
+    <Grid/>
     </>
   )
 }
---
+++ src/types/task.ts
@@ -0,0 +1,6 @@
+export default interface Task{
+    name:string
+    status:string
+    priority:string
+    description:string
+}
